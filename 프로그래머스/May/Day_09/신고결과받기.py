5ec846f2a814ecfab3c969294f00e669837075b2
vvoo_ds <48902356+JungWooChul@users.noreply.github.com>
2023-05-09 18:18:33 +0900

    Update 신고결과받기.py

--- 프로그래머스/May/Day_09/신고결과받기.py
+++ 프로그래머스/May/Day_09/신고결과받기.py
@@ -1,1 +1,14 @@
+def solution(n):
+    answer = 0
+    arr = list(range(2,n+1))
 
+    for i in arr:
+        tmp = 0
+        for j in range(2, int(i**(1/2)) + 1):
+            if i%j == 0:
+                tmp += 1
+                break
+        if tmp == 0:
+            answer += 1
+
+    return answer
